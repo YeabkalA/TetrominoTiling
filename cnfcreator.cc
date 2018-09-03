877bf89e56c55b7a8033ed7358dfe9a39e40d7b3
Yeabkal <ywubshit@udallas.edu>
2018-09-03 13:16:11 -0500

    Update cnfcreator.cc

--- cnfcreator.cc
+++ cnfcreator.cc
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <initializer_list>
+#include <fstream>
 
 // Orientation for a tetromino
 enum TetrominoOrientation { UP, DOWN, LEFT, RIGHT };
@@ -31,33 +32,33 @@ private:
 class Tetromino {
 public:
   Tetromino(TetrominoOrientation orntn, int startingLoc,
-	    Dimension dimension) : gridDimension(dimension),
-				   start(startingLoc),
-				   orientation(orntn) {
+   Dimension dimension) : gridDimension(dimension),
+  start(startingLoc),
+  orientation(orntn) {
     int width = dimension.Width();
 
     // Cover the appropriate cells based on the orientation
     // and starting location of the tetromino.
     switch(orntn) {
-    case UP:
+      case UP:
       coveredCells[0] = startingLoc;
       coveredCells[1] = startingLoc + 1;
       coveredCells[2] = startingLoc + 2;
       coveredCells[3] = startingLoc - width + 1;
       break;
-    case LEFT:
+      case LEFT:
       coveredCells[0] = startingLoc;
       coveredCells[1] = startingLoc + width;
       coveredCells[2] = startingLoc + 2*width;
       coveredCells[3] = startingLoc + width - 1;
       break;
-    case RIGHT:
+      case RIGHT:
       coveredCells[0] = startingLoc;
       coveredCells[1] = startingLoc + width;
       coveredCells[2] = startingLoc + 2*width;
       coveredCells[3] = startingLoc + width + 1;
       break;
-    case DOWN:
+      case DOWN:
       coveredCells[0] = startingLoc;
       coveredCells[1] = startingLoc + 1;
       coveredCells[2] = startingLoc + 2;
@@ -84,18 +85,18 @@ public:
 
     if (orientation == RIGHT) {
       return start % gridDimension.Width() != 0
-	&& gridDimension.Size() - start >= 2*gridDimension.Width(); 
+      && gridDimension.Size() - start >= 2*gridDimension.Width(); 
     } else if (orientation == LEFT) {
       return (start - 1) % gridDimension.Width() !=0
-	&& gridDimension.Size() - start >= 2*gridDimension.Width();
+      && gridDimension.Size() - start >= 2*gridDimension.Width();
     } else if (orientation == UP) {
       return (start > gridDimension.Width())
-	&& (((start + 1) % gridDimension.Width()) != 0)
-	&& (start % gridDimension.Width() != 0);
+      && (((start + 1) % gridDimension.Width()) != 0)
+      && (start % gridDimension.Width() != 0);
     } else {
       return (gridDimension.Size() - start < gridDimension.Width())
-	&& gridDimension.Width() != 0
-	&& start % gridDimension.Width() != 0;
+      && gridDimension.Width() != 0
+      && start % gridDimension.Width() != 0;
     }
   }
 
@@ -108,7 +109,7 @@ public:
   void Print() {
     std::cout << "Orientation " << orientation << ", covered cells are:\n";
     for (int cell: coveredCells) { std::cout << cell << "-"; }
-    std::cout << std::endl;
+      std::cout << std::endl;
     std::cout << "-------------------------\n";
   }
 
@@ -126,66 +127,110 @@ private:
 class Grid {
 public:
   Grid(Dimension d, std::vector<int>* avoided)
-    : dimension(d), index(0) {
+  : dimension(d), index(0) {
     if(avoided != nullptr) {
       for(int cell: *avoided) { avoidedCells.push_back(cell); }
     }
-  }
+}
 
-  Grid(int w, int h, std::vector<int>* avoided)
-    : dimension(Dimension(w,h)), index(0) {
-    if (avoided != nullptr) { 
-      for(int cell: *avoided) { avoidedCells.push_back(cell); }
-    }
+Grid(int w, int h, std::vector<int>* avoided)
+: dimension(Dimension(w,h)), index(0) {
+  if (avoided != nullptr) { 
+    for(int cell: *avoided) { avoidedCells.push_back(cell); }
   }
+}
 
   // Map each valid cell to all the tetrominos that cover it.
-  void ConstructMap() {
-    for (auto t: tetrominos) {
-      for (int cell: t.coveredCells) {
-        if(cellMap.find(cell) == cellMap.end()) {
-          cellMap.insert(std::pair<int, std::vector<Tetromino> >(cell, std::vector<Tetromino>()));
-        }
-        cellMap[cell].push_back(t);
-      }
-    }
-  }
-
-  // Populate list of all possibly used tetrominos.
-  void FillTetrominosList() {
-    for (int i = 1; i <= dimension.Size(); i++) {
-      if (std::find(avoidedCells.begin(), avoidedCells.end(), i) != avoidedCells.end()) {
+void ConstructMap() {
+  for (auto t: tetrominos) {
+    for (int cell: t.coveredCells) {
+      if (cell < 1 || cell > dimension.Size()) {
         continue;
       }
-      Tetromino t[] = {Tetromino(UP, i, dimension),
-		       Tetromino(DOWN, i, dimension),
-		       Tetromino(LEFT, i, dimension),
-		       Tetromino(RIGHT, i, dimension)};
-      for(Tetromino tetr: t) {
-	if (tetr.IsValid(&avoidedCells)) {
-	  tetr.SetIndex(index++);
-	  tetrominos.push_back(tetr);
-	}
+      if(cellMap.find(cell) == cellMap.end()) {
+        cellMap.insert(std::pair<int, std::vector<Tetromino> >(cell, std::vector<Tetromino>()));
+      }
+      cellMap[cell].push_back(t);
+    }
+  }
+}
+
+  // Populate list of all possibly used tetrominos.
+void FillTetrominosList() {
+  for (int i = 1; i <= dimension.Size(); i++) {
+    if (std::find(avoidedCells.begin(), avoidedCells.end(), i) != avoidedCells.end()) {
+      continue;
+    }
+    Tetromino t[] = {Tetromino(UP, i, dimension),
+     Tetromino(DOWN, i, dimension),
+     Tetromino(LEFT, i, dimension),
+     Tetromino(RIGHT, i, dimension)};
+     for(Tetromino tetr: t) {
+       if (tetr.IsValid(&avoidedCells)) {
+         tetr.SetIndex(index++);
+         tetrominos.push_back(tetr);
+       }
+     }
+   }
+ }
+
+ void AddAvoidedCell(int ind) { avoidedCells.push_back(ind); }
+
+ void GenerateCoveredClauses(int* size) {
+  for (auto it=cellMap.begin(); it!=cellMap.end(); ++it) {
+    coveredClauses.push_back("c 'COVERED' " + std::to_string(it->first));
+    auto tetrs = it->second;
+    std::string out;
+    for (auto t: tetrs) {
+      out = out + std::to_string(t.CNFIndex()) + " ";
+    }
+    out = out + " 0";
+    coveredClauses.push_back(out);
+    *size = *size + 1;
+  }
+}
+
+void GenerateOnceClauses(int* size) {
+  int count = 0;
+  for (auto it=cellMap.begin(); it!=cellMap.end(); ++it) {
+    auto tetrs = it->second;
+    for(auto t1: tetrs) {
+      for(auto t2: tetrs) {
+        onceClauses.push_back(std::to_string(-1 * t1.CNFIndex()) + " "
+          + std::to_string(-1 * t2.CNFIndex()) + " 0");
+        *size = *size + 1;
       }
     }
   }
+}
 
-  void AddAvoidedCell(int ind) { avoidedCells.push_back(ind); }
+void GenereateCNFFile(int size, const std::string& fileName) {
+  std::ofstream cnf_file;
+  cnf_file.open(fileName);
 
-  void GenerateCoveredClauses() {
-    
+  std::string first_line = "p " + std::to_string(tetrominos.size())
+  + " " + std::to_string(size);
+
+  cnf_file << first_line << std::endl;
+  for (const std::string& clause: coveredClauses) {
+    cnf_file << clause << std::endl;
   }
-
-  void Print() {
-    for(auto i = cellMap.begin(); i != cellMap.end(); i++) {
-      std::cout << i->first << "-->\n";
-      auto list = cellMap[i->first];
-      for (Tetromino t: list) {
-	t.Print();
-      }
-      std::cout << "\n**************\n";
-    }
+  for (const std::string& clause: onceClauses) {
+    cnf_file << clause << std::endl;
   }
+  cnf_file.close();
+}
+
+void Print() {
+  for(auto i = cellMap.begin(); i != cellMap.end(); i++) {
+    std::cout << i->first << "-->\n";
+    auto list = cellMap[i->first];
+    for (Tetromino t: list) {
+     t.Print();
+   }
+   std::cout << "\n**************\n";
+ }
+}
 
 private:
   // Map of each cell to all tetrominos covering it.
@@ -194,6 +239,10 @@ private:
   Dimension dimension;
   // List of all possible tetrominos for the grid.
   std::vector<Tetromino> tetrominos;
+  // Covered Clauses.
+  std::vector<std::string> coveredClauses;
+  // Once Clauses.
+  std::vector<std::string> onceClauses;
   // Index (cnf index) of a tetromino to be added.
   // Constantly updated as new tetrominos are added
   // since the cnf index of a tetromino is unique.
@@ -211,4 +260,10 @@ int main() {
   grid.FillTetrominosList();
   grid.ConstructMap();
   grid.Print();
+
+  int clauseSize = 0;
+  grid.GenerateCoveredClauses(&clauseSize);
+  grid.GenerateOnceClauses(&clauseSize);
+  grid.GenereateCNFFile(clauseSize, "first.cnf");
+  // grid.GenerateCoveredClauses();
 }
