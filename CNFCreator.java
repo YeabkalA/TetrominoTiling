f3d69eae64c9d688ca7bab621f06879d6eb9c6cf
Yeabkal <ywubshit@udallas.edu>
2018-09-06 23:39:52 -0500

    Update CNFCreator.java

--- CNFCreator.java
+++ CNFCreator.java
@@ -315,7 +315,8 @@ public class CNFCreator {
 				b = new BufferedReader(f);
 				while((line = b.readLine()) != null) {
 					if(line.equals("SAT")) continue;
-					int[] tempSol = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+					int[] tempSol = int[] sol = Arrays.stream(line.split(" ")).
+							mapToInt(Integer::parseInt).filter(cell -> cell > 0).toArray();
 					for(int i = 0; i < tempSol.length - 1; i++) { solution.add(tempSol[i]); }
 				}
 			} catch(IOException e) {}
